056c1ec0c77ce4e51fdc9059490e4549b5f33f1c
Diana <dianaserik0@gmail.com>
2024-05-24 13:27:45 +0500

    step 4 complete

--- index.js
+++ index.js
@@ -13,15 +13,26 @@ const gendiff = (path1, path2) => {
     const keys1 = Object.keys(data1);
     const keys2 = Object.keys(data2);
     const sortedKeys = _.union(keys1, keys2).sort();
-    console.log(sortedKeys);
+    //console.log(sortedKeys);
 
     const diff = sortedKeys.reduce((acc, key) => {
         if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
             acc.push(` - ${key}: ${data1[key]}`)
-        }
+        } else if (Object.hasOwn(data2, key) && !Object.hasOwn(data1, key)) {
+            acc.push(` + ${key}: ${data2[key]}`)
+        }  else if (Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
+            if (data1[key] === data2[key]) {
+                acc.push(`   ${key}: ${data2[key]}`)
+            } else if (data1[key] !== data2[key]) {
+                acc.push(` - ${key}: ${data1[key]}`)
+                acc.push(` + ${key}: ${data2[key]}`)
+            } 
+         }  
         return acc;
     }, ['{'])
-    console.log(diff);
+    diff.push('}');
+    const result = diff.join('\n');
+    console.log(result);
 }
 
 export default gendiff;
